a03875044ed3116d142e695df46b2752fe39d10f
Gaurav <patilgaurav029@gmail.com>
2024-06-28 20:00:01 -0500

    added multiprocessing for feature extraction

--- extractor.py
+++ extractor.py
@@ -1,33 +1,39 @@
+import cv2
 from keras.preprocessing import image
 from keras.applications.inception_v3 import InceptionV3, preprocess_input
-from keras.models import Model, load_model
-from keras.layers import Input
+from keras.models import Model
 import numpy as np
+from multiprocessing import Pool
+
+def process_frame(frame_path):
+    img = image.load_img(frame_path, target_size=(299, 299))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    return x
 
 class Extractor():
     def __init__(self):
-
         self.weights = None 
-        
         base_model = InceptionV3(
             weights='imagenet',
             include_top=True
         )
 
-        # extract features at the final pool layer.
         self.model = Model(
             inputs=base_model.input,
             outputs=base_model.get_layer('avg_pool').output
         )
 
+    def extract_features(self, frame_paths, batch_size=32):
+        with Pool() as p:
+            processed_frames = p.map(process_frame, frame_paths)
+        features = []
+        for i in range(0, len(processed_frames), batch_size):
+            batch = np.vstack(processed_frames[i:i+batch_size])
+            batch_features = self.model.predict(batch)
+            features.extend(batch_features)
+        return np.array(features)
 
     def extract(self, image_path):
-        img = image.load_img(image_path, target_size=(299, 299))
-        x = image.img_to_array(img)
-        x = np.expand_dims(x, axis=0)
-        x = preprocess_input(x)
-
-        # Get the prediction.        
-        features = self.model.predict(x)
-
-        return features[0]
+        return self.extract_features([image_path])[0]
--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import cv2
 import matplotlib.pyplot as plt
-# from matplotlib import pyplot as plt
-from flask import Flask, Response, render_template, request, redirect , jsonify
-from flask_mail import Mail
-from flask_mail import Message
+from flask import Flask, render_template, request, redirect
+from flask_mail import Mail, Message
 import os
 import glob
 import numpy as np
@@ -12,7 +10,6 @@ from keras.models import load_model
 import sys
 from subprocess import call
 import shutil
-from matplotlib import animation
 import matplotlib
 from keras import backend as K
 import string 
@@ -32,11 +29,10 @@ app.config.update(dict(
 ))
 mail = Mail(app)
 
-# Before prediction
 K.clear_session()
 
-inception = Extractor()  # load pre-trained inception model
-saved_model = "__path_to_model__" # "data/checkpoints/lstm-features.001-0.000.hdf5
+inception = Extractor()
+saved_model = "/home/gaurav/Downloads/eye_in_the_sky_latest/checkpoints/lstm-features-final.keras"
 model = load_model(saved_model)
 
 @app.route("/")
@@ -45,65 +41,45 @@ def home():
 
 @app.route("/analyze", methods=["POST"])
 def analyze():
-    print(request.form["video"])
     if request.form["video"] == "":
         print("No args! exiting")
         return redirect("/")
-    # Before prediction
-    # K.clear_session()
 
-    # shape that lstm expects is (40, 2048)
     seq_length = 40
 
     fname_ext = request.form["video"]
     file_path = "static/sample_videos/" + fname_ext
     fname = fname_ext.split('.')[0]
-    print(fname)
-    print(file_path)
-    # print(sys.argv[1])
     
-    # Check if the file exists
     if not os.path.isfile(file_path):
         print(f"File {file_path} does not exist.")
         return redirect("/")
 
-
     call(["ffmpeg", "-i", file_path, "-r", str(seq_length),
           os.path.join('static/extracted_frames', fname + '-%04d.jpg')])
 
-    # data = DataSet(seq_length=40, class_limit=2)
     frames = sorted(glob.glob(os.path.join(
-        'static/extracted_frames', fname + '*jpg')))
+        'static/extracted_frames', fname + '*jpg'))) 
 
-    # frames = sorted(glob.glob("static/extracted_frames/temp*jpg"))
-
-    # make sure number of frames is a multiple of seq_length
     nframes = len(frames)-(len(frames) % seq_length)
-
-    # remove extra frames
     frames = frames[: nframes]
     
     if len(frames) == 0:
         print("No frames extracted.")
         return redirect("/")
 
-
     x = [i for i in range(0, nframes//seq_length)]
-    # x = np.linspace(0, nframes//40, nframes//40)
     y_violent = []
     y_non_violent = []
+
+    # Extract features for all frames at once
+    all_features = inception.extract_features(frames)
+
     for i in range(0, nframes, seq_length):
-        sequence = []
-        for frame in frames[i: i+seq_length]:
-            features = inception.extract(frame)
-            sequence.append(features)
+        sequence = all_features[i:i+seq_length]
         prediction = model.predict(np.expand_dims(sequence, axis=0))
-        # prediction[0][0] is non violent score
-        # prediction[0][1] is violent score
-        # print(prediction)
         y_violent.append(prediction[0][1])
         y_non_violent.append(prediction[0][0])
-        # np.save('data/saved_sequence/' + fname, sequence)
 
     print(x)
     print(y_violent)
@@ -121,35 +97,18 @@ def analyze():
             start_frame_pos = 1
         end_frame_pos = start_frame_pos + 5
     
-    total_score = 0
-    for i in range(len(y_violent)):
-        print(y_violent[i])
-        total_score = total_score + y_violent[i]
-    avg_violence_score = total_score/len(y_violent)
-    print("avg_violence_score",avg_violence_score)
+    avg_violence_score = sum(y_violent) / len(y_violent)
+    print("avg_violence_score", avg_violence_score)
 
-    # plt.step(x, y_violent, label='violence score')
-    # plt.step(x, y_non_violent, label='non-violent score')
     plt.plot(x, y_violent, 'r', label='violence-score')
-    # plt.plot(x, y_non_violent, 'b', label='non-violence-score')
-
     plt.xlabel('time(s)')
     plt.ylabel('violence')
     plt.title('Violence in video')
     plt.ylim(0, 1)
     plt.legend()
     plt.savefig('static/plots/' + fname + '.png')
-    # plt.savefig('static/plots/temp.png')
-
-    # plt.show()
     plt.close()
 
-    # clean up by deleting frames captured
-    # shutil.rmtree('data/extracted_frames')
-    # os.makedirs('data/extracted_frames')
-
-    # After prediction
-    # K.clear_session()
     if(avg_violence_score>=0.85):
         msg = Message("Violence Detected", sender="hellomaneeshp@gmail.com", recipients=["__recipient__email"])
         msg.html = "<h3>Real Time Violence Detection System Alert</h3>"
@@ -166,14 +125,11 @@ def analyze():
                 with app.open_resource("static/extracted_frames/" + frame_name +".jpg") as fp:
                     msg.attach("frame.jpg", "image/jpg", fp.read())
             #mail.send(msg)
-            relative_file_path = os.path.join('sample_videos', fname_ext)     
+    relative_file_path = os.path.join('sample_videos', fname_ext)     
     return render_template("analyze.html", file_path=relative_file_path, fname=fname, y_violent=y_violent)
-    # return render_template("analyze.html", y_violent=y_violent)
-
 
 @app.route("/live", methods=["POST"])
 def live():
-    # shape that lstm expects is (40, 2048)
     seq_length = 40      
         
     chars = string.ascii_letters
@@ -181,37 +137,8 @@ def live():
     
     fname = 'live'.join(random.choice(chars) for x in range(size))
 
-    # print result 
     print("The generated random file name : " + str(fname)) 
 
-     # for windows use this command to capture video from webcam using ffmpeg
-    '''call(
-    [
-        "ffmpeg",
-        "-f",
-        "dshow",
-        "-i",
-        "video=HD WebCam",
-        "-r",
-        "40",
-        "-t",
-        "10",
-        "static/extracted_frames/" + fname + "-%04d.jpg",
-    ]
-    )
-    
-    # for linux use this command to capture video from webcam using ffmpeg
-    call(
-    [
-        "ffmpeg",
-        "-f", "v4l2",
-        "-i", "/dev/video0",
-        "-r", "40",
-        "-t", "10",
-        f"static/extracted_frames/{fname}-%04d.jpg",
-    ]
-    )'''
-    
     video_path = f'static/live_videos/{fname}.mp4'
     call(
     [
@@ -223,7 +150,6 @@ def live():
     ]
     )
 
-    # Extract frames from the recorded video
     call(
     [
         "ffmpeg",
@@ -236,51 +162,34 @@ def live():
     frames = sorted(glob.glob(os.path.join(
         'static/extracted_frames', fname + '*jpg')))
 
-    # make sure number of frames is a multiple of seq_length
     nframes = len(frames)-(len(frames) % seq_length)
-
-    # remove extra frames
     frames = frames[: nframes]
 
     x = [i for i in range(0, nframes//seq_length)]
-    # x = np.linspace(0, nframes//40, nframes//40)
     y_violent = []
     y_non_violent = []
+
+    # Extract features for all frames at once
+    all_features = inception.extract_features(frames)
+
     for i in range(0, nframes, seq_length):
-        sequence = []
-        for frame in frames[i: i+seq_length]:
-            features = inception.extract(frame)
-            sequence.append(features)
+        sequence = all_features[i:i+seq_length]
         prediction = model.predict(np.expand_dims(sequence, axis=0))
-        # prediction[0][0] is non violent score
-        # prediction[0][1] is violent score
-        # print(prediction)
         y_violent.append(prediction[0][1])
         y_non_violent.append(prediction[0][0])
-        # np.save('data/saved_sequence/' + fname, sequence)
 
     print(x)
     print(y_violent)
     print(y_non_violent)
 
-    # plt.step(x, y_violent, label='violence score')
-    # plt.step(x, y_non_violent, label='non-violent score')
     plt.plot(x, y_violent, 'r', label='violence-score')
-    # plt.plot(x, y_non_violent, 'b', label='non-violence-score')
-
     plt.xlabel('time(s)')
     plt.ylabel('violence')
     plt.title('Violence in video')
     plt.ylim(0, 1)
     plt.legend()    
     plt.savefig('static/plots/' + fname + '.png')
-
-    # plt.show()
     plt.close()
-
-    # clean up by deleting frames captured
-    # shutil.rmtree('data/extracted_frames')
-    # os.makedirs('data/extracted_frames')
 
     return render_template("live.html", fname=fname,video_path=video_path, y_violent=y_violent)
 
