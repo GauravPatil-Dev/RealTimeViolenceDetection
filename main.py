23901a33a8e6f0663096be6fc8a6315709409aa6
Gaurav <patilgaurav029@gmail.com>
2024-06-30 11:21:39 -0500

    added real time streaming

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+from flask_mail import Message
+from flask_mail import Mail
+
 import cv2
 import matplotlib.pyplot as plt
-from flask import Flask, render_template, request, redirect
-from flask_mail import Mail, Message
+from flask import Flask, render_template, request, redirect, jsonify
+from flask_socketio import SocketIO, emit
 import os
 import glob
 import numpy as np
@@ -12,31 +15,27 @@ from subprocess import call
 import shutil
 import matplotlib
 from keras import backend as K
-import string 
-import random 
+import string
+import random
+import base64
 
 matplotlib.use('Agg')
 
 app = Flask(__name__)
 app.config.update(dict(
     DEBUG=True,
-    MAIL_SERVER='smtp.gmail.com',
-    MAIL_PORT=587,
-    MAIL_USE_TLS=True,
-    MAIL_USE_SSL=False,
-    MAIL_USERNAME='__recipient__email',
-    MAIL_PASSWORD='__recipient__password',
+    SECRET_KEY='secret!',
 ))
-mail = Mail(app)
+socketio = SocketIO(app)
 
 K.clear_session()
 
 inception = Extractor()
-saved_model = "__path_to_saved_model__"
+saved_model = "/home/gaurav/Pictures/eye_in_the_sky_latest/checkpoints/lstm-features-final.keras"
 model = load_model(saved_model)
 
 @app.route("/")
-def home():    
+def home():
     return render_template("home.html")
 
 @app.route("/analyze", methods=["POST"])
@@ -50,7 +49,7 @@ def analyze():
     fname_ext = request.form["video"]
     file_path = "static/sample_videos/" + fname_ext
     fname = fname_ext.split('.')[0]
-    
+
     if not os.path.isfile(file_path):
         print(f"File {file_path} does not exist.")
         return redirect("/")
@@ -59,11 +58,11 @@ def analyze():
           os.path.join('static/extracted_frames', fname + '-%04d.jpg')])
 
     frames = sorted(glob.glob(os.path.join(
-        'static/extracted_frames', fname + '*jpg'))) 
+        'static/extracted_frames', fname + '*jpg')))
 
     nframes = len(frames)-(len(frames) % seq_length)
     frames = frames[: nframes]
-    
+
     if len(frames) == 0:
         print("No frames extracted.")
         return redirect("/")
@@ -96,7 +95,7 @@ def analyze():
         if(start_frame_pos == 0):
             start_frame_pos = 1
         end_frame_pos = start_frame_pos + 5
-    
+
     avg_violence_score = sum(y_violent) / len(y_violent)
     print("avg_violence_score", avg_violence_score)
 
@@ -112,86 +111,113 @@ def analyze():
     if(avg_violence_score>=0.85):
         msg = Message("Violence Detected", sender="hellomaneeshp@gmail.com", recipients=["__recipient__email"])
         msg.html = "<h3>Real Time Violence Detection System Alert</h3>"
-        with app.open_resource("static/plots/" + fname +".png") as fp:
+        with app.open_resource("static/plots/" + fname + ".png") as fp:
             msg.attach("violence_score_plot.png", "image/png", fp.read())
             for i in range(start_frame_pos+1,end_frame_pos+1):
                 if(i < 10):
-                    name = "000" + str(i) 
+                    name = "000" + str(i)
                 if(i >=10 and i<100):
-                    name = "00" + str(i) 
+                    name = "00" + str(i)
                 if(i>=100):
-                    name = "0" + str(i) 
-                frame_name = fname + "-" + name        
-                with app.open_resource("static/extracted_frames/" + frame_name +".jpg") as fp:
+                    name = "0" + str(i)
+                frame_name = fname + "-" + name
+                with app.open_resource("static/extracted_frames/" + frame_name + ".jpg") as fp:
                     msg.attach("frame.jpg", "image/jpg", fp.read())
             #mail.send(msg)
-    relative_file_path = os.path.join('sample_videos', fname_ext)     
+    relative_file_path = os.path.join('sample_videos', fname_ext)
     return render_template("analyze.html", file_path=relative_file_path, fname=fname, y_violent=y_violent)
 
 @app.route("/live", methods=["POST"])
 def live():
-    seq_length = 40      
-        
+    return render_template("live.html")
+
+@socketio.on('start_stream')
+def start_stream():
     chars = string.ascii_letters
     size = 5
-    
-    fname = 'live'.join(random.choice(chars) for x in range(size))
+    fname = 'live' + ''.join(random.choice(chars) for x in range(size))
 
-    print("The generated random file name : " + str(fname)) 
+    print("The generated random file name : " + str(fname))
 
+    cap = cv2.VideoCapture(0)
     video_path = f'static/live_videos/{fname}.mp4'
-    call(
-    [
-        "ffmpeg",
-        "-f", "v4l2",
-        "-i", "/dev/video0",
-        "-t", "5",
-        video_path,
-    ]
-    )
+    out = cv2.VideoWriter(video_path, cv2.VideoWriter_fourcc(*'mp4v'), 20.0, (640,480))
+
+    frame_count = 0
+    while frame_count < 100:
+        ret, frame = cap.read()
+        if ret:
+            out.write(frame)
+            frame_count += 1
+            _, buffer = cv2.imencode('.jpg', frame)
+            frame_encoded = base64.b64encode(buffer).decode('utf-8')
+            emit('frame', {'frame': frame_encoded})
+        else:
+            break
+
+    cap.release()
+    out.release()
+
+    print(f"Video saved at path: {video_path}")
+
+    emit('stream_complete', {'video_path': f'../{video_path}'})
+
+@socketio.on('process_stream')
+def process_stream(data):
+    seq_length = 40
+    video_path = data['video_path'].replace('../', '')
+
+    print(f"Processing video at path: {video_path}")
+
+    fname = os.path.basename(video_path).split('.')[0]
 
     call(
-    [
-        "ffmpeg",
-        "-i", video_path,
-        "-r", str(seq_length),
-        f"static/extracted_frames/{fname}-%04d.jpg",
-    ]
+        [
+            "ffmpeg",
+            "-i", video_path,
+            "-r", str(seq_length),
+            f"static/extracted_frames/{fname}-%04d.jpg",
+        ]
     )
 
-    frames = sorted(glob.glob(os.path.join(
-        'static/extracted_frames', fname + '*jpg')))
+    frames = sorted(glob.glob(f'static/extracted_frames/{fname}-*.jpg'))
+
+    print(f"Number of frames: {len(frames)}")
 
     nframes = len(frames)-(len(frames) % seq_length)
     frames = frames[: nframes]
+
+    print(f"Number of frames after adjustment: {nframes}")
 
     x = [i for i in range(0, nframes//seq_length)]
     y_violent = []
     y_non_violent = []
 
     # Extract features for all frames at once
-    all_features = inception.extract_features(frames)
+    if frames:
+        all_features = inception.extract_features(frames)
 
-    for i in range(0, nframes, seq_length):
-        sequence = all_features[i:i+seq_length]
-        prediction = model.predict(np.expand_dims(sequence, axis=0))
-        y_violent.append(prediction[0][1])
-        y_non_violent.append(prediction[0][0])
+        for i in range(0, nframes, seq_length):
+            sequence = all_features[i:i+seq_length]
+            prediction = model.predict(np.expand_dims(sequence, axis=0))
+            y_violent.append(prediction[0][1])
+            y_non_violent.append(prediction[0][0])
 
-    print(x)
-    print(y_violent)
-    print(y_non_violent)
+    print(f"x: {x}")
+    print(f"y_violent: {y_violent}")
+    print(f"y_non_violent: {y_non_violent}")
 
     plt.plot(x, y_violent, 'r', label='violence-score')
     plt.xlabel('time(s)')
     plt.ylabel('violence')
     plt.title('Violence in video')
     plt.ylim(0, 1)
-    plt.legend()    
-    plt.savefig('static/plots/' + fname + '.png')
+    plt.legend()
+    plot_path = f'static/plots/{fname}.png'
+    plt.savefig(plot_path)
     plt.close()
 
-    return render_template("live.html", fname=fname,video_path=video_path, y_violent=y_violent)
+    emit('plot_complete', {'plot_path': f'../{plot_path}'})
 
-if __name__ == "__main__":    
-    app.run(debug=False, threaded=False)
+if __name__ == "__main__":
+    socketio.run(app, host='0.0.0.0', port=5000, debug=False)
